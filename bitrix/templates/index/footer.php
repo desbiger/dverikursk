7052a15c0a73f4b73a771b92c78f16f86d259e79
Pavel <desbiger@gmail.com>
2013-12-03 21:15:41 +0400

    нижнее дуюлирующее меню

--- bitrix/templates/index/footer.php
+++ bitrix/templates/index/footer.php
@@ -1,5 +1,23 @@
+<div class="clear"></div>
+<?$APPLICATION->IncludeComponent("bitrix:menu", "bottom_menu", array(
+	"ROOT_MENU_TYPE" => "top",
+	"MENU_CACHE_TYPE" => "N",
+	"MENU_CACHE_TIME" => "3600",
+	"MENU_CACHE_USE_GROUPS" => "Y",
+	"MENU_CACHE_GET_VARS" => array(
+	),
+	"MAX_LEVEL" => "1",
+	"CHILD_MENU_TYPE" => "left",
+	"USE_EXT" => "N",
+	"DELAY" => "N",
+	"ALLOW_MULTI_SELECT" => "N"
+	),
+	false
+);?>
+
 <? if ($_SERVER['PHP_SELF'] == "/index.php"): ?>
 	<div class = "recommend">
+
 		<h3>Мы рекомендуем</h3>
 		<?$GLOBALS['arrFilter'] = array("PROPERTY_REKOMENDUEM_VALUE" => 'да')?>
 		<?$APPLICATION->IncludeComponent("bitrix:catalog.section", "rekomenduem", array(
